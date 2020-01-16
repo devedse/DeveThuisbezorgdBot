d11ec8b9eb0e118d806a65688926ae0d15c85f46
Devedse <davydavidse@gmail.com>
2020-01-16 16:44:38 +0100

    Fixed unit test

---
+++ DeveThuisbezorgdBot.Tests/SampleTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace DeveThuisbezorgdBot.Tests
+{
+    public class SampleTests
+    {
+        [Fact]
+        public void SampleTest123()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.True(1 == 1);
+        }
+    }
+}
